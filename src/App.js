c6cca32882829b202d2e6c6f3ba0bfd5d037046f
Atiti1 <atitidestiny17@gmail.com>
2021-10-24 15:03:06 +0100

    contact app to  display name and number

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState} from "react";
+import "./App.css";
 
 function App() {
+   const [myname, setMyname] = useState('');
+   const [mynumber, setMynumber] = useState('');
+   const [item, setItem ] = useState([]);
+
+  //  const stateArr = useState("");
+  //  const state = stateArr[0];
+  //  const setState = stateArr[1];
+   
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>MY CONTACT LIST</h1>
+      <input type="search" placeholder="name" onChange={event => setMyname(event.target.value)}/>
+      <input type="search" placeholder="phone number" onChange={event => setMynumber(event.target.value)}/>
+      <button onClick={() => {
+        const store = [...item]
+       // store.push(myname);
+        store.push({myname: myname, mynumber: mynumber})
+        setItem(store)
+        console.log(item)
+
+        
+        
+        // const Num = [ ...mynumber ] 
+        // Num.push(mynumber);
+        // setMynumber(Num)
+      
+        //console.log(mynumber)
+      }}> SAVE</button>
+      <div>
+        {item.map((quan) => {
+          return <div><h1>{quan.myname}</h1><h2>{quan.mynumber}</h2></div>
+        })}
+    
+      </div>
     </div>
   );
 }
